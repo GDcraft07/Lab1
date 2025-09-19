0287d4ffadee58d2fb5b8e898e3427c7311ada72
gdcraft07 <iklass987654321@gmail.com>
2025-09-19 19:32:44 +0300

    lab code

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,7 +6,51 @@ public class Main {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = new PrintStream(System.out);
 
-    public static void main(String[] args) {
-        out.println("Hello world");
+    public static void main(String[] args) throws IOException{
+        double X = in.nextDouble(), A = in.nextDouble(), B = in.nextDouble(), C = in.nextDouble();
+
+        if (X - A < 0) {
+            if (X - B < 0) {
+                if (X - C < 0){
+                    out.println(0);
+                }
+
+                else {
+                    out.println(1);
+                }
+            }
+
+            else {
+                if (X - (B + C) < 0) {
+                    out.println(1);
+                }
+
+                else {
+                    out.println(2);
+                }
+            }
+        }
+
+        else {
+            if (X - (A + B) < 0) {
+                if (X - (A + C) < 0) {
+                    out.println(1);
+                }
+
+                else {
+                    out.println(2);
+                }
+            }
+
+            else {
+                if (X - (A + B + C) < 0) {
+                    out.println(2);
+                }
+
+                else {
+                    out.println(3);
+                }
+            }
+        }
     }
 }
