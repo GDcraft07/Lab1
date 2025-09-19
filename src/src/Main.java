04e6af79836f5d865432bf31e4f083e4693e09f9
gdcraft07 <iklass987654321@gmail.com>
2025-09-19 18:58:05 +0300

    add src, main class and test start

---
+++ src/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.io.IOException;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = new PrintStream(System.out);
+
+    public static void main(String[] args) {
+        out.println("Hello world");
+    }
+}
