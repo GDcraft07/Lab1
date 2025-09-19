7f5708a77d24a4586bc9f69b5cfee4a79f44e0f1
gdcraft07 <iklass987654321@gmail.com>
2025-09-19 22:39:13 +0300

    add file Main.java without comments

---
+++ src/src/WithoutComments.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
+
+public class WithoutComments {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = new PrintStream(System.out);
+
+    public static void main(String[] args) {
+        double X = in.nextDouble(), A = in.nextDouble(), B = in.nextDouble(), C = in.nextDouble();
+
+        if (X - A < 0) {
+            if (X - B < 0) {
+                if (X - C < 0){
+                    out.println(0);
+                }
+
+                else {
+                    out.println(1);
+                }
+            }
+
+            else {
+                if (X - B - C < 0) {
+                    out.println(1);
+                }
+
+                else {
+                    out.println(2);
+                }
+            }
+        }
+
+        else {
+            if (X - A - B < 0) {
+                if (X - A - C < 0) {
+                    out.println(1);
+                }
+
+                else {
+                    out.println(2);
+                }
+            }
+
+            else {
+                if (X - A - B - C < 0) {
+                    out.println(2);
+                }
+
+                else {
+                    out.println(3);
+                }
+            }
+        }
+    }
+}
